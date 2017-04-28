65cad022ba4c933fb6cee54db97ee624149262de
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-27 17:32:09 -0700

    update photo

--- client/templates/photos.js
+++ client/templates/photos.js
@@ -4,7 +4,20 @@
         var file = $('#myphoto').get(0).files[0];
         
         if (file){
-            toastr.erro("No file uploaded");
+            fsFile = new FS.File(file);
+            Photos.insert(fsFile, function(err, result){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    toastr.success('File uploaded');
+                    Router.go('/');
+                }
+            });
+        }
+        else
+        {
+            toastr.error("No file uploaded");
             Router.go('/add');
         }
         return false;
